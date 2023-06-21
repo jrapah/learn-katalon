9f8932b7218f98295f1eb14cb5b75c0a03f3c842
jrapah <46885683+jrapah@users.noreply.github.com>
2023-06-21 22:18:25 +0700

    create test suites

---
+++ Test Suites/FullTest.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FullTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>474f2898-11f4-422c-84df-ea829f0fda50</testSuiteGuid>
+</TestSuiteEntity>
